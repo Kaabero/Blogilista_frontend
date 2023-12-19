cdcfcb901a03d1963539c427b625288d8134c2c9
Katri <katri.kuoppala@helsinki.fi>
2023-12-19 13:06:54 +0200

    5.7 blogilistan frontend, step7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    Title: {blog.title} - Author: {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showingVisible, setShowingVisible] = useState(false)
+  const hideWhenVisible = { display: showingVisible ? 'none' : '' }
+  const showWhenVisible = { display: showingVisible ? '' : 'none' }
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+    <div style={blogStyle}>  
+      <div>
+        Title: {blog.title} <br>
+        </br>
+        <div style={hideWhenVisible}>
+        <button onClick={() => setShowingVisible(true)}>view</button>
+        </div>
+        <div style={showWhenVisible}>
+        Author: {blog.author} <br>
+        </br>
+        URL: {blog.url} <br>
+        </br>
+        likes: {blog.likes} <button onClick={() => addLike()}>like</button><br>
+        </br>
+        <button onClick={() => setShowingVisible(false)}>hide</button>
+        </div>
+      </div>
+    </div>  
+)}
+
 
 export default Blog
