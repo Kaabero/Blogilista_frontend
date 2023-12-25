f79704be3d0c60dd754efdc3c4d8915daf01cf22
Katri <katri.kuoppala@helsinki.fi>
2023-12-25 20:44:58 +0200

    5.15: blogilistan testit, step3

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -57,4 +57,37 @@ test('renders blog author, url, likes and user after clicking view button', asyn
     expect(screen.getByText(blog.likes, {exact: false}))
     expect(screen.getByText(blog.user.name, {exact: false}))
   })
+
+  test('clicking the like button calls event handler twice', async () => {
+    const blog = {
+        title: 'Test Blog',
+        author: 'Test Author',
+        url: 'www.katri.fi',
+        likes: 1,
+        user: {
+          name: 'Test User',
+          username: 'testuser',
+          id: 1
+        },
+      }
+    
+    const mockAddLike = jest.fn()
+    const mockRemove = jest.fn()
+    
+    render(
+        <Blog blog={blog} addLike={mockAddLike} remove={mockRemove} loggedUser="testuser" />
+    )
+    
+
+  
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+    
+  
+    expect(mockAddLike.mock.calls).toHaveLength(2)
+  })
   
