eb4f5da33bb2e54f7ce4dce8fc2cf4f157a53ade
Katri <katri.kuoppala@helsinki.fi>
2023-12-29 20:00:40 +0200

    5.18: blogilistan end to end ‑testit, step2

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -14,4 +14,29 @@ describe('Blog app', function() {
     it('Login form is shown', function() {
       cy.contains('log in to application')
     })
-  })
+
+    
+    describe('Login',function() {
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('testuser')
+            cy.get('#password').type('testpassword')
+            cy.get('#login-button').click()
+
+            cy.contains('Test User logged in')
+          
+        })
+        
+        it('fails with wrong credentials', function() {
+            
+            cy.get('#username').type('wrongusername')
+            cy.get('#password').type('testpassword')
+            cy.get('#login-button').click()
+        
+            cy.contains('wrong username or password')
+        })
+          
+    })
+        
+    
+      
+})
